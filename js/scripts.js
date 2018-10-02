69ef0aac2a59eb31b4e2a4291211d2332fb9c126
Jacob Rachal <jacob_rachal@mail.fresnostate.edu>
2018-10-01 21:02:54 -0700

    Performed code changes when asked by project lead.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-// Jacob here, found something which might work.
 // In the html, give the section/header/div the following id to show the clock:: 'clock'
 // For example: <h1 class="text-center" id="clock"></h1>
 
@@ -6,41 +5,38 @@
 // (will need at least 2: one for the titles and one for the actual description)
 
 // PUT THE boolean VALUE HERE (for preventing the users from spamming the "refresh" feature)
-var hammertime = true;
+let clear_to_refresh = true;
 
 // PUT THE ARRAY REFRESHER HERE
 // (need to dig up the randomizer selector from previous class or find one with internet)
 function array_refresh (){
-  console.log("Wait, is this midnight or early morning?"); //just for checking that this works as intended with displayClock func.
+  //do stuff here.
 };
 
-
 //HERE BEGINS THE CODE FOR THE COUNTDOWN CLOCK ---------------------------------
-function checkTime (i) {
-  if (i < 10){i= "0" +i};
+function time_segments (i) {
+  if (i < 10){i= `0${i}`;};
   return i;
 }; // makes our clock more normal/symmetric-looking.
 
-function displayClock(){
+function display_clock(){
   const clock = document.getElementById("clock");
   // Jacob here, I really hope this is the best way of going about it.
   setInterval(function () {
-    var today = new Date();
-    var hour = 24 - today.getHours();
-    var min = 60 - today.getMinutes();
-    var sec = 60 - today.getSeconds();
+    let today = new Date();
+    let hour = 24 - today.getHours();
+    let min = 60 - today.getMinutes();
+    let sec = 60 - today.getSeconds();
     //putting a checker to see if it's time to load up a new checklist of nice things to do.
     if( (hour === 0) && (min===0) && (sec===0) ){/* call ^^^THAT FUNCTION here, and don't stop running this function. */
-      console.log("Successfully caught the midnight hour!");
       array_refresh();
       //also, reset the 'do_more' boolean to TRUE (note: do this inside the array refresher)
     };
-    hour = checkTime(hour);
-    min = checkTime(min);
-    sec = checkTime(sec);
+    hour = time_segments(hour);
+    min = time_segments(min);
+    sec = time_segments(sec);
     clock.textContent = `${hour}:${min}:${sec}`;
-
-  }, 1000);//callback function.
+  }, 1000);
 };
-displayClock();
+display_clock();
 //HERE ENDS THE CODE FOR THE COUNTDOWN CLOCK. ----------------------------------
