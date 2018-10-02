3bceba1270f4e166bb5afdd1aef2d7f04fe859b9
Jacob Rachal <jacob_rachal@mail.fresnostate.edu>
2018-10-01 21:08:58 -0700

    Camel casing

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,17 @@ let clear_to_refresh = true;
 
 // PUT THE ARRAY REFRESHER HERE
 // (need to dig up the randomizer selector from previous class or find one with internet)
-function array_refresh (){
+function arrayRefresh (){
   //do stuff here.
 };
 
 //HERE BEGINS THE CODE FOR THE COUNTDOWN CLOCK ---------------------------------
-function time_segments (i) {
+function timeSegments (i) {
   if (i < 10){i= `0${i}`;};
   return i;
 }; // makes our clock more normal/symmetric-looking.
 
-function display_clock(){
+function displayClock(){
   const clock = document.getElementById("clock");
   // Jacob here, I really hope this is the best way of going about it.
   setInterval(function () {
@@ -29,14 +29,14 @@ function display_clock(){
     let sec = 60 - today.getSeconds();
     //putting a checker to see if it's time to load up a new checklist of nice things to do.
     if( (hour === 0) && (min===0) && (sec===0) ){/* call ^^^THAT FUNCTION here, and don't stop running this function. */
-      array_refresh();
+      arrayRefresh();
       //also, reset the 'do_more' boolean to TRUE (note: do this inside the array refresher)
     };
-    hour = time_segments(hour);
-    min = time_segments(min);
-    sec = time_segments(sec);
+    hour = timeSegments(hour);
+    min = timeSegments(min);
+    sec = timeSegments(sec);
     clock.textContent = `${hour}:${min}:${sec}`;
   }, 1000);
 };
-display_clock();
+displayClock();
 //HERE ENDS THE CODE FOR THE COUNTDOWN CLOCK. ----------------------------------
