48f3c0c72d3528e7283015118ac7b8e0a509a339
Jasmine Carbajal <jaz@Jasmines-MacBook-Pro.local>
2018-10-09 14:35:07 -0700

    updated progress circle. score animation works. yay

--- js/scripts.js
+++ js/scripts.js
@@ -43,6 +43,12 @@ function displayClock(){
 
 
 // this is the progress circle
+// circle siziing
+var canvas = document.getElementsByTagName('myCanvas')[0];
+myCanvas.width  = 202;
+myCanvas.height = 202;
+myCanvas.style.width  = '255px';
+myCanvas.style.height = '255px';
 
 var ctx = document.getElementById('myCanvas').getContext('2d');
 var al = 0;
@@ -51,18 +57,20 @@ var cw = ctx.canvas.width;
 var ch = ctx.canvas.height;
 var diff;
 
+
 function progressSim (){
   diff = ((al / 100) * Math.PI*2*10).toFixed(2);
   ctx.clearRect(0, 0, cw,  ch);
   ctx.lineWidth = 10;
-  ctx.fillStyle = '#fc4a1a';
-  ctx.strokeStyle = '#f7b733';
-  ctx.textAline = 'center';
-  ctx.fillText = (al +'%', cw*.5, ch*.5+2, cw);
+  ctx.fillStyle = '#00000';
+  ctx.strokeStyle = '#ff7141';
+  ctx.font = '17px happy monkey'
+  ctx.textAlign = 'center';
+  ctx.fillText (al +'/100', cw*.5, ch*.5+2, cw);
   ctx.beginPath();
-  ctx.arc(100, 75, 50, start, diff/10+start, false);
+  ctx.arc(100, 95, 90, start, diff/10+start, false);
   ctx.stroke();
-  if(al>=100){
+  if(al>=75){
     clearTimeout(sim);
   }
   al++;
