bd8ba9bcd93bef09a738682cafb46d23204a0dc0
Jasmine Carbajal <jaz@Jasmines-MacBook-Pro.local>
2018-10-15 20:02:11 -0700

    cleaned up js and html

--- js/scripts.js
+++ js/scripts.js
@@ -39,12 +39,12 @@ function displayClock(){
   }, 1000);
 };
 displayClock();
-// displayClock();
+
 //HERE ENDS THE CODE FOR THE COUNTDOWN CLOCK. ----------------------------------
 
 
 // this is the progress circle
-// circle siziing
+// circle sizing
 let canvas = document.getElementsByTagName('myCanvas')[0];
 myCanvas.width  = 202;
 myCanvas.height = 202;
@@ -52,28 +52,26 @@ myCanvas.style.width  = '255px';
 myCanvas.style.height = '255px';
 
 let ctx = document.getElementById('myCanvas').getContext('2d');
-let al = 0;
-let start = 4.72;
-let cw = ctx.canvas.width;
-let ch = ctx.canvas.height;
-let diff;
-
+let kindPointCounter = 0;
 
 function progressSim (){
-  diff = ((al / 100) * Math.PI*2*10).toFixed(2);
+  let cw = ctx.canvas.width;
+  let ch = ctx.canvas.height;
+  let start = 4.72;
+  let diff = (( kindPointCounter / 100) * Math.PI*2*10).toFixed(2);
   ctx.clearRect(0, 0, cw,  ch);
   ctx.lineWidth = 10;
   ctx.fillStyle = '#00000';
   ctx.strokeStyle = '#ff7141';
   ctx.font = '17px happy monkey'
   ctx.textAlign = 'center';
-  ctx.fillText (al +'/100', cw*.5, ch*.5+2, cw);
+  ctx.fillText (kindPointCounter +'/100', cw*.5, ch*.5+2, cw);
   ctx.beginPath();
   ctx.arc(100, 95, 90, start, diff/10+start, false);
   ctx.stroke();
-  if(al>=75){
+  if(kindPointCounter>=75){
     clearTimeout(sim);
   }
-  al++;
+  kindPointCounter++;
 }
 let sim = setInterval (progressSim, 50);
